698a66e4379f3f61a2fd0969e62a950f2969e705
willisxiong <xiongwei.fly@gmail.com>
2022-02-20 00:38:50 +0800

    Create main.tf

---
+++ ap-east-1/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-east-1"
+}
+
+resource "aws_instance" "vm1" {
+  ami = "ami-0eddb0d9c3bfc43ca"
+  subnet_id = "subnet-0d4a59a27527fac85"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-vm1"
+  }
+}
