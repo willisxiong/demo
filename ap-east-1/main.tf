0af8042ce75522975e83d2658dde33e777bd72cc
willisxiong <xiongwei.fly@gmail.com>
2022-02-20 00:41:53 +0800

    change vm name

--- ap-east-1/main.tf
+++ ap-east-1/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "ap-east-1"
 }
 
-resource "aws_instance" "vm1" {
+resource "aws_instance" "vm" {
   ami = "ami-0eddb0d9c3bfc43ca"
   subnet_id = "subnet-0d4a59a27527fac85"
   instance_type = "t3.micro"
   tags = {
-    Name = "my-vm1"
+    Name = "my-vm"
   }
 }
