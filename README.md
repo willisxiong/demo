8b1f71040de08a32a2a9e248c275ebe73568675a
willisxiong <xiongwei.fly@gmail.com>
2022-02-20 11:34:46 +0800

    change vm type to t2.micro

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Test ssh connection to Github and push code
 Willis macbook connect to github
 
 develop a new feature in branch "refactor"
+
+the refactor done
--- ap-east-1/main.tf
+++ ap-east-1/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "vm" {
   ami = "ami-0eddb0d9c3bfc43ca"
   subnet_id = "subnet-0d4a59a27527fac85"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
     Name = "my-vm"
   }
